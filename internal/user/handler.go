d593b5559616cce5f05c4008dd108b83f1941e0a
Sokorinjo <vsokorac65@gmail.com>
2025-04-07 16:24:13 +0200

    Added getAllUsers.

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -11,29 +11,29 @@ import (
 )
 
 type User struct {
-	ID   int64
-	Name string
-	Pass string
+	ID      int64
+	Name    string
+	Pass    string
+	Created string
 }
 
 // Create user
 func createUser(w http.ResponseWriter, r *http.Request) {
 	var user User
 
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	res, err := db.DB.Exec("INSERT INTO users (user_name, user_pass) VALUES(?, ?)", user.Name, user.Pass)
+	_, err := db.DB.Exec("INSERT INTO users (user_name, user_pass) VALUES(?, ?)", user.Name, user.Pass)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error inserting user: %s", err), http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(res)
+	w.Write([]byte("User successfuly created."))
 }
 
 // Get specific user
@@ -44,7 +44,7 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	var user User
 
 	//Make a query to DB
-	err := db.DB.QueryRow("SELECT user_id, user_name,user_pass created_at FROM users WHERE user_id = ?", userId).Scan(&user.ID, &user.Name, &user.Pass)
+	err := db.DB.QueryRow("SELECT user_id, user_name, user_pass, created_at FROM users WHERE user_id = ?", userId).Scan(&user.ID, &user.Name, &user.Pass, &user.Created)
 	if err != nil {
 		fmt.Println(err)
 		http.Error(w, "User not found", http.StatusNotFound)
@@ -55,7 +55,33 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	
+}
+
 // Get all users
 func getAllUsers(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get all users."))
+	rows, err := db.DB.Query("SELECT * FROM users;")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var users []User
+	for rows.Next() {
+		var user User
+		if err := rows.Scan(&user.ID, &user.Name, &user.Pass, &user.Created); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
